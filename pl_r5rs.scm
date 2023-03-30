7e34e9f7aab2ef77651e52a270597b28977f5702
c0np4nn4 <c0np4nn4@gmail.com>
2023-03-30 09:54:15 +0900

    Update pl_r5rs.scm

--- pl_r5rs.scm
+++ pl_r5rs.scm
@@ -1,4 +1,20 @@
 (define (double n)
   (+ n n))
 
-(double 1234)
+(define twice (lambda (n) (+ n n)))
+
+(define n 7)
+
+; (double 1234)
+(double n)
+
+; (lambda (x y) (+ x y)
+((lambda (x y) (+ x y)) n 10)
+(twice 5)
+
+(define f
+  (lambda (x)
+    (if (> x 0) x (- x))))
+
+(f 123)
+(f -1910)
